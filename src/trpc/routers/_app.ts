93bc2a77ba7145f40df82a54dd5fcbdfcbb2b748
MFurkan40 <mfurkan1140@gmail.com>
2025-02-14 23:59:42 +0300

    added video categories

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,18 +1,11 @@
-import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "../init";
+// Router Modules
+import { categoriesRouter } from "@/modules/categories/server/procedures";
+
+// TRPC
+import { createTRPCRouter } from "../init";
+
 export const appRouter = createTRPCRouter({
-  hello: protectedProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query((opts) => {
-      console.log({ dbUser: opts.ctx.user });
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+  categories: categoriesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
